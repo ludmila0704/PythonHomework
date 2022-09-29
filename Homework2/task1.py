449eeaeb0b31711af51879983d9d34b5f9da5e62
Ludmila <ludmila0704@yandex.ru>
2022-09-29 18:07:32 +0300

    Homework2

--- Homework2/task1.py
+++ Homework2/task1.py
@@ -3,33 +3,31 @@
 # 6782 -> 23
 # 0,56 -> 11
 print('Введите число:')
-
 number = float(input())
-#выделяем целую часть числа
-#if float.is_integer( number ):
-if number % 1==0:
-    number=int(number)
-    summ=0
-    while (number > 0):
-        summ = int(summ + number % 10)
-        number = number / 10
-    print(f' Сумма цифр числа равна {int(summ)} ')
-else:
-    d = (round( number % 1),2 )
-    print(f'd  {d} ')
-    summ=0
-   # while(d!=0):
-    #d = int(( number % 1) * 10)
-    d = (d * 10)
-    print(f'd  {d} ')
-        
-    #summ=summ+d
-    #d=d-int(d)
-    print(f'd  {d} ')
 
+#считаем сумму цифр целой части числа 
+numberint=int(number)
+summint=0
+while (numberint > 0):
+    summint = int(summint + numberint % 10)
+    numberint = numberint / 10
+print(f' Сумма цифр числа равна {int(summint)} ')
+#считаем сумму цифр дробной части числа 
+d = number % 1
+d.as_integer_ratio()
 
-
-    print(f'Сумма цифр числа равна  {d} равна {summ}')
-#not isinstance(number,float)
-
+summ_ost=0
+d1=d
+while(d1!=0 and d1!=9):
+    
+    d1 = int(d * 10) # целое   
+    summ_ost=summ_ost+d1
+    d2=d*10-d1
+    d=d2
+    if int(d2*10)==9:#пайтон добавляет 9, поэтому это условие
+        summ_ost+=1
+        break
+#если входная 9 считает неверно
+summ=summint+summ_ost
+print(f'Сумма цифр числа {number} равна {summ}')
 
